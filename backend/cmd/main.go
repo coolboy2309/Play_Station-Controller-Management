162e34949c2af4498d06e6cbd0e020c6852387c3
Abenezer Abadi <abenu755@gmail.com>
2026-07-02 17:59:17 +0300

    feat: implement database module

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"zoneeye/backend/internal/config"
+	"zoneeye/backend/internal/database"
 	"zoneeye/backend/internal/logger"
 )
 
@@ -19,9 +20,21 @@ func main() {
 
 	logger.Info("Configuration Loaded")
 
+	logger.Info("Connecting database...")
+
+	err = database.Connect(config.AppConfig.Database)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+
+	logger.Info("Database Connected Successfully")
+
+	fmt.Println("================================")
 	fmt.Println("Server :", config.AppConfig.ServerName)
 	fmt.Println("Version:", config.AppConfig.Version)
 	fmt.Println("Host   :", config.AppConfig.Host)
 	fmt.Println("Port   :", config.AppConfig.Port)
 	fmt.Println("DB     :", config.AppConfig.Database)
+	fmt.Println("================================")
 }
---
+++ backend/internal/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
+var DB *sql.DB
+
+func Connect(path string) error {
+
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return err
+	}
+
+	if err = db.Ping(); err != nil {
+		return err
+	}
+
+	DB = db
+
+	return nil
+}
